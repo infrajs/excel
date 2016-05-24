a1d27df23aede8e4b1d59154dfc73849dfed5345
Anton <a.kiyatkin@itlf.ru>
2016-05-24 15:12:36 +0400

    fix tester

--- tester.php
+++ tester.php
@@ -15,16 +15,24 @@ $ans = array();
 //1 Минимум требований к структуре Excel документа, данные "как есть"
 $data = Xlsx::parse('vendor/infrajs/excel/test.xlsx');
 
-if (sizeof($data) != 19) return Ans::err($ans, 'Некорректный результат Xlsx::parse '.sizeof($data));
+
+if (sizeof($data) != 22) return Ans::err($ans, 'Некорректный результат Xlsx::parse '.sizeof($data));
+
 
 
 //2 Простая структура - Распознаются заголовки таблицы, описание таблицы, структура групп. Можно применять Xlsx::runPoss и Xlsx::runGroups
 $data = Xlsx::get('vendor/infrajs/excel/test.xlsx');
+
+
 if (sizeof($data) != 7) return Ans::err($ans, 'Некорректный результат Xlsx::get '.sizeof($data));
 
 
 //3 Оптимизировання структура. Обязательна колонка Артикул, объединение групп. Большой список опций вторым аргументом
 $data = Xlsx::init('vendor/infrajs/excel/test.xlsx');
+/*echo count($data);
+echo '<pre>';
+print_r($data);
+exit;*/
 if (sizeof($data['childs']) != 1) return Ans::err($ans, 'Некорректный результат Xlsx::init '.sizeof($data));
 
 
